9ed72cb8f22eb841f39b0f87c7a96775ad0c589f
HomerDilpleu <petitgigi78@yahoo.fr>
2024-08-15 16:05:52 +0200

    Little collision refactoring

--- src/sprites/platform.js
+++ src/sprites/platform.js
@@ -27,39 +27,30 @@ game.sprites.platform.clone = function (_width,_height,X,Y) {
 
 game.sprites.platform.checkPlayerCollision = function () {
     let _p = game.sprites.player
-    let _HBR = game.sprites.player.hitBoxRight
-    let _HBL = game.sprites.player.hitBoxLeft
-    let _HBU = game.sprites.player.hitBoxUp
-    let _HBD = game.sprites.player.hitBoxDown
     // Min and Max of current sprite
     let _x1Min = this.X-this.width / 2
     let _x1Max = this.X+this.width / 2
     let _y1Min = this.Y-this.height / 2
     let _y1Max = this.Y+this.height / 2
     // RIGHT hit box
-    let _x2Min = _HBR.Xmin
-    let _x2Max = _HBR.Xmax
-    let _y2Min = _HBR.Ymin
-    let _y2Max = _HBR.Ymax
-    if (!(_x1Min>_x2Max || _x2Min>_x1Max || _y1Min>_y2Max || _y2Min>_y1Max)) {game.sprites.player.collidesRight = true} 
+    if (game.sprites.platform.checkHitBoxCollision(_x1Min, _x1Max, _y1Min, _y1Max, game.sprites.player.hitBoxRight)) {
+        _p.collidesRight = true
+    } 
     // LEFT hit box
-    _x2Min = _HBL.Xmin
-    _x2Max = _HBL.Xmax
-    _y2Min = _HBL.Ymin
-    _y2Max = _HBL.Ymax
-    if (!(_x1Min>_x2Max || _x2Min>_x1Max || _y1Min>_y2Max || _y2Min>_y1Max)) {game.sprites.player.collidesLeft = true} 
+    if (game.sprites.platform.checkHitBoxCollision(_x1Min, _x1Max, _y1Min, _y1Max, game.sprites.player.hitBoxLeft)) {
+        _p.collidesLeft = true
+    } 
     // UP hit box
-    _x2Min = _HBU.Xmin
-    _x2Max = _HBU.Xmax
-    _y2Min = _HBU.Ymin
-    _y2Max = _HBU.Ymax
-    if (!(_x1Min>_x2Max || _x2Min>_x1Max || _y1Min>_y2Max || _y2Min>_y1Max)) {game.sprites.player.collidesUp = true} 
+    if (game.sprites.platform.checkHitBoxCollision(_x1Min, _x1Max, _y1Min, _y1Max, game.sprites.player.hitBoxUp)) {
+        _p.collidesUp = true
+    } 
     // DOWN hit box
-    _x2Min = _HBD.Xmin
-    _x2Max = _HBD.Xmax
-    _y2Min = _HBD.Ymin
-    _y2Max = _HBD.Ymax
-    if (!(_x1Min>_x2Max || _x2Min>_x1Max || _y1Min>_y2Max || _y2Min>_y1Max)) {game.sprites.player.collidesDown = true} 
+    if (game.sprites.platform.checkHitBoxCollision(_x1Min, _x1Max, _y1Min, _y1Max, game.sprites.player.hitBoxDown)) {
+        _p.collidesDown = true
+    } 
+}
 
+game.sprites.platform.checkHitBoxCollision = function (_x1Min, _x1Max, _y1Min, _y1Max, _hitBox) {
+    if (!(_x1Min>_hitBox.Xmax || _hitBox.Xmin>_x1Max || _y1Min>_hitBox.Ymax || _hitBox.Ymin>_y1Max)) {return true} 
 }
 
