8daa077aaaecce60be79de9efcf2d2fe79d763b5
HomerDilpleu <petitgigi78@yahoo.fr>
2024-08-25 23:31:14 +0200

    Add pi in the text box

--- src/sprites/textBox.js
+++ src/sprites/textBox.js
@@ -4,18 +4,25 @@ game.sprites.textBox.init = function() {
     this.width = 800
     this.height = 50
     this.x = mge.game.width/2
-    this.y = this.height/2
+    this.y = this.height/2 + 5
     this._text = ''
 }
 
 game.sprites.textBox.drawFunction = function (ctx) {
     if (this._text != '') {
-        ctx.fillStyle = '#390052'
+        // Box
+        ctx.fillStyle = 'white'
+        ctx.strokeStyle = 'black'
+        ctx.lineWidth = 2
         ctx.fillRect(0,0,this.width,this.height)
+        ctx.strokeRect(0,0,this.width,this.height)
+        // Text
         ctx.font = '24px serif'
         ctx.textAlign = 'center'
         ctx.textBaseline = 'middle'
-        ctx.fillStyle = '#ccafdf'
+        ctx.fillStyle = 'black'
         ctx.fillText(this._text, this.width/2, this.height/2)
+        // Pi
+        game.images.pi.draw(ctx)
     }
 }
